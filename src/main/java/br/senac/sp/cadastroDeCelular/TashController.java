e1900233fe92fed51f40f0a950328525a7ea9855
Gabriel Nicolau de Sousa <gabrielnicolllau1@gmail.com>
2024-05-03 22:15:46 -0300

    Adicionando o CSS

--- src/main/java/br/senac/sp/cadastroDeCelular/TashController.java
+++ src/main/java/br/senac/sp/cadastroDeCelular/TashController.java
@@ -1,6 +1,10 @@
 package br.senac.sp.cadastroDeCelular;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,13 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("task")
 public class TashController {
 
+    @Autowired
+    TaskRepository repository;
+
     @GetMapping
-    public String index() {
-        
-        // Nome da pagina ser exibida
+    public String index(Model model) {
+
+        List<Task> tasks = repository.findAll();
+       // var tasks = repository.findAll(); // findAll retorna uma lista de tarefas
+
+        // Passando uma lista de tarefas do banco de dados
+        model.addAttribute("tasks", tasks);
+
         return "task/index";
-
-
 
     }
 }
